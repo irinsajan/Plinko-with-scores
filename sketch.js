94a1bf8047b4848f2e6ed2dd33123a75f1c3ff3e
irinsajan <69659076+irinsajan@users.noreply.github.com>
2021-02-16 16:24:12 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,26 +14,8 @@ function setup() {
   engine = Engine.create();
   world = engine.world;
   ground = new Ground(width/2,height,width,20);
- 
 
-    
-}
- 
-
-
-function draw() {
-  background("black");
-  textSize(20)
- //text("Score : "+score,20,30);
-  Engine.update(engine);
-
-  for (var k = 0; k <=width; k = k + 80) {
-    divisions = [k, height-divisionHeight/2, 10, divisionHeight];
-    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
-  }
-
-
-   for (var j = 75; j <=width; j=j+50) 
+  for (var j = 75; j <=width; j=j+50) 
    {
    
       plinkos.push(new Plinko(j,75));
@@ -56,7 +38,29 @@ function draw() {
    
       plinkos.push(new Plinko(j,375));
    }
+
+   
+  for (var k = 0; k <=width; k = k + 80) {
+    
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
  
+ 
+
+    
+}
+ 
+
+
+function draw() {
+  background("black");
+  textSize(20)
+ //text("Score : "+score,20,30);
+  Engine.update(engine);
+
+
+
+   
   
    for (var i = 0; i < plinkos.length; i++) {
      
@@ -64,17 +68,17 @@ function draw() {
      
    }
    if(frameCount%60===0){
-     particle = [random(width/2-30, width/2+30), 10,10];
+     
      particle.push(new Particle(random(width/2-30, width/2+30), 10,10));
      score++;
    }
  
   for (var j = 0; j < particle.length; j++) {
    
-     particle.pop();
+     particle[j].display();
    }
    for (var k = 0; k < divisions.length; k++) {
      
-     divisions.pop();
+     divisions[k].display();
    }
 }
